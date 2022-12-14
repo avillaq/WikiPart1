762ab0a63984b046f0686f8b654eef32077e1b83
alex <avillafuerteq@unsa.edu.pe>
2022-12-14 02:06:58 -0500

    Conexion con la base de datos

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -2,10 +2,22 @@
 use strict;
 use warnings;
 use CGI;
+use DBI;
 
 my $q = CGI->new;
 my $titulo = $q->param('titulo');
 my $texto = $q->param('texto');
+$texto =~ s/\n/<br>/g;
+
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=paginasDB;host=192.168.1.23";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar");
+
+my $sth = $dbh->prepare("INSERT INTO paginas(title, text)VALUES(?,?)");
+$sth->execute($titulo,$texto);
+$sth->finish;
+$dbh->disconnect;
 
 print $q->header('text/html');
 print<<HTML;
@@ -16,7 +28,7 @@ print<<HTML;
 </head>
 <body>
     <h1>$titulo</h1>
-    <p>$texto</p>
+    $texto
     <hr>
 
     <h2>Pagina grabada <a href="./list.pl">Listado de Paginas</a></h2>
