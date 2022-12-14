ecd2e4b822973ecfcfaf575dbe8923306f6ecb43
alex <avillafuerteq@unsa.edu.pe>
2022-12-13 23:06:53 -0500

    Creamos list.pl

---
+++ cgi-bin/list.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+
+print<<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Wikipedia 0.1</title>
+</head>
+<body>
+    <h1>Nuestras paginas de wiki</h1>
+    <p>Aqui va la lista de paginas</p>
+    <hr>
+    <a href="../new.html">Nueva Pagina</a><br>
+    <a href="../index.html">Volver al Inicio</a>
+</body>
+</html>
+HTML
